b27b68767790249cbf2b8f4d04923bbb32232a79
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-03-12 11:08:20 +0800

    Test linstaged

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,9 @@ import express from 'express'
 
 const app = express();
 
+console.log('test here')
+console.log('test two..')
+
 app.listen( 8000, () => {
     console.log('Server is running on PORT: 8000')
 })
