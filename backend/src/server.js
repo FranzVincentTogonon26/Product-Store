26e96459769aa4f7310f685043ad89c18f4de28f
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-03-12 11:09:53 +0800

    Test Three

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 console.log('test here')
 console.log('test two..')
+console.log('test three')
 
 app.listen( 8000, () => {
     console.log('Server is running on PORT: 8000')
